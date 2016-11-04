c7526d76ccd9c3c80f7188d63f7ad410a27b1d4f
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-04 18:24:16 +0100

    Start of Lab #1.

---
+++ src/js224eh_lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+package js224eh_lab1;
+
+/**
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-04.
+ */
+public class HelloWorld {
+   public static void main(String[] args) {
+       System.out.println("Hello World!");
+   }
+}
