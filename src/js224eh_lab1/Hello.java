9462d3939301e6fefb9cd13b1b1f92ba245a318b
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-04 18:24:58 +0100

    Rename 'HelloWorld.java' to 'Hello.java'.

--- src/js224eh_lab1/Hello.java
+++ src/js224eh_lab1/Hello.java
@@ -3,7 +3,7 @@ package js224eh_lab1;
 /**
  * Created by Jonas Sjöberg (js224eh) on 2016-11-04.
  */
-public class HelloWorld {
+public class Hello {
    public static void main(String[] args) {
        System.out.println("Hello World!");
    }
