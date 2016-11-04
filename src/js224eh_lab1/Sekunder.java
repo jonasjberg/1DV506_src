8cd9a748b0164941ddcc9daf05ddcee2d40331af
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-04 21:01:26 +0100

    Add skeleton "L2 7. Antal sekunder" ..

---
+++ src/js224eh_lab1/Sekunder.java
@@ -0,0 +1,26 @@
+package js224eh_lab1;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-04.
+ *
+ * Lektion 2 - (Input/Output, Operations on Primitive Types)
+ *
+ * 7. Antal sekunder
+ *    Skriv ett program Sekunder.java som läser in tre heltal (timmar, minuter,
+ *    sekunder) och sedan beräknar motsvarande tid i sekunder.
+ *    Tex, 1 timme, 28 minuter och 42 sekunder är samma sak som 5322 sekunder.
+ *    En exekvering kan se ut enligt följande:
+ *
+ *     Timmar: 1
+ *     Minuter: 28
+ *     Sekunder: 42
+ *
+ *     Totala antalet sekunder: 5322
+ */
+public class Sekunder
+{
+    public static void main(String[] args)
+    {
+        /* TODO: Implement .. */
+    }
+}
