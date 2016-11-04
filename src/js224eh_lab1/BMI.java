145fb16b555184665083db768386de516201ea91
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-04 22:50:26 +0100

    Add skeleton for "L2 - 8. BMI" ..

---
+++ src/js224eh_lab1/BMI.java
@@ -0,0 +1,27 @@
+package js224eh_lab1;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-04.
+ *
+ * Lektion 2 - (Input/Output, Operations on Primitive Types)
+ *
+ * 8. BMI
+ *    Skriv ett program BMI.java som räknar ut BMI (Body Mass Index) för en
+ *    person. Programmet skall läsa in data (längd och vikt) från tangentbordet
+ *    och sedan presentera svaret. BMI räknas ut som vikt/(längd)^2 där längden
+ *    ges i meter och vikten ges i kilogram. En exekvering kan se ut som följer:
+ *
+ *        Ange din längd i meter: 1,83
+ *        Ange din vikt i kilogram: 83
+ *        Ditt BMI är: 25
+ *
+ *    Notera: BMI är alltid ett heltal.
+ */
+
+public class BMI
+{
+    public static void main(String[] args)
+    {
+        /* TODO: .. */
+    }
+}
