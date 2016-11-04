fcf9b82f76c0c8dbfb7a245ac798d53920526dcc
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-04 19:39:38 +0100

    Done with "L2 - 5. Utskrifter" ..

--- src/js224eh_lab1/Print.java
+++ src/js224eh_lab1/Print.java
@@ -9,41 +9,92 @@ public class Print {
     public static void main(String[] args) {
         printStringOnOneLine(STR_KNOWLEDGE_POWER);
         printSplitStringOnThreeLines(STR_KNOWLEDGE_POWER);
-        printSplitStringOnThreeLines("Kunskap är makt!");
-        printSplitStringOnThreeLines("Kunskap är makt! Test");
-        printSplitStringOnThreeLines("Kunskap är makt! Test mjao");
+        printStringSurroundedByRectangle(STR_KNOWLEDGE_POWER);
     }
 
+    /**
+     * Prints a string on one line.
+     *
+     * @param stringToPrint The string to print. Must be non-empty.
+     */
     private static void printStringOnOneLine(String stringToPrint) {
+        if (stringToPrint == null || stringToPrint.isEmpty()) {
+            return;
+        }
+
         System.out.println(stringToPrint);
     }
 
     /**
      * Prints a string on three lines with one word per line.
-     *
-     * The string is split on whitespace with a maximum of three splits. If the
-     * string can be split into more than three parts, the third parts will
-     * contain more than one whitespace-separated word.
+     * <p>
+     * The string is split on whitespace with a maximum of three splits.
+     * If the string can be split into more than three parts, the third
+     * part will contain more than one whitespace-separated word.
      *
      * @param stringToPrint The string to split and print.
      */
     private static void printSplitStringOnThreeLines(String stringToPrint) {
         /* Reference for String method 'split':
-         * http://docs.oracle.com/javase/7/docs/api/java/lang/String.html#split(java.lang.String,%20int)
+         * http://docs.oracle.com/javase/7/docs/api/java/lang/String
+         * .html#split(java.lang.String,%20int)
          *
          * Reference for regular expression pattern:
-         * https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
+         * https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern
+         * .html
          *
          * Regex pattern used:    \s    A whitespace character: [ \t\n\x0B\f\r]
          *                        X+    X, one or more times
          */
 
+        if (stringToPrint == null || stringToPrint.isEmpty()) {
+            return;
+        }
+
         String[] subStrings = stringToPrint.split("\\s+", 3);
         for (int i = 0; i < subStrings.length; i++) {
-            if (subStrings[i] == null) {
-                continue;
-            }
             System.out.println(subStrings[i]);
+        }
+    }
+
+    /**
+     * Prints a string surrounded by a rectangle made from characters '=' and
+     * '|'.
+     *
+     * @param stringToPrint The string to print.
+     */
+    private static void printStringSurroundedByRectangle(String stringToPrint) {
+        if (stringToPrint == null || stringToPrint.isEmpty() ||
+            stringToPrint.length() < 1) {
+            return;
+        }
+
+        final String CHAR_HORIZ = "=";
+        final String CHAR_VERTI = "|";
+
+        // +2 for the whitespace "padding".
+        int rectangleWidth = stringToPrint.length() + CHAR_HORIZ.length()
+                             + CHAR_VERTI.length() + 2;
+
+        printStringNtimes(CHAR_HORIZ, rectangleWidth);
+        System.out.println("\n" + CHAR_VERTI + " " + stringToPrint + " " +
+                           "" + "" + CHAR_VERTI);
+        printStringNtimes(CHAR_HORIZ, rectangleWidth);
+    }
+
+    /**
+     * Prints a string n times, without adding whitespace or line breaks.
+     *
+     * @param stringToPrint The string to print.
+     * @param n             Number of times the string is printed. Must be a
+     *                      positive integer.
+     */
+    public static void printStringNtimes(String stringToPrint, int n) {
+        if (n < 0) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(stringToPrint);
         }
     }
 }
