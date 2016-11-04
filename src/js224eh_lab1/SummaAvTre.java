0076e0221d7e0c89f429095aefa790abef86a875
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-04 23:43:35 +0100

    Add skeleton "L2 - 10. Summa av tre siffror" ..

---
+++ src/js224eh_lab1/SummaAvTre.java
@@ -0,0 +1,27 @@
+package js224eh_lab1;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-04.
+ *
+ * Lektion 2 - (Input/Output, Operations on Primitive Types)
+ *
+ * 10. Summa av tre siffror
+ *
+ *     Skriv ett program SummaAvTre.java där användaren ombeds mata in ett
+ *     tresiffrigt tal. Programmet ska sedan bilda summan av de siffrorna
+ *     heltalet utgörs av, samt skriva ut summan. Det tresiffriga talet måste
+ *     läsas in i sin helhet vid ett och samma tillfälle.
+ *
+ *         Ange ett tresiffrigt heltal: 483
+ *         Summan av siffrorna i heltalet är: 15
+ *
+ *     Ledtråd: Använd modulus (%10) och heltalsdivision (/10) för att plocka
+ *              ut siffrorna en åt gången.
+ */
+public class SummaAvTre
+{
+    public static void main(String[] args)
+    {
+        /* TODO .. */
+    }
+}
