ad8b0c8edbaf5a0adb868a24c0a11715e37f3459
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-04 23:54:23 +0100

    Update 'SummaAvTre' ..

--- src/js224eh_lab1/SummaAvTre.java
+++ src/js224eh_lab1/SummaAvTre.java
@@ -1,5 +1,7 @@
 package js224eh_lab1;
 
+import java.util.Scanner;
+
 /*
  * Created by Jonas Sjöberg (js224eh) on 2016-11-04.
  *
@@ -22,6 +24,53 @@ public class SummaAvTre
 {
     public static void main(String[] args)
     {
-        /* TODO .. */
+        final String MSG_PROMPT_THREE_DIGITS = "Ange ett tresiffrigt heltal: ";
+
+        int threeDigitNumber = promptUserForThreeDigitInt(MSG_PROMPT_THREE_DIGITS);
+
+        System.out.println(threeDigitNumber);
+    }
+
+    /**
+     * Prints a message to the user.
+     *
+     * @param message The message to print. Must not be empty.
+     */
+    private static void queryUser(String message)
+    {
+        if (message != null && message.length() > 0) {
+            System.out.print(message);
+        }
+    }
+
+    /**
+     * Prompts the user for a number.
+     *
+     * The message is shown continuously until the user has entered a valid
+     * number. Any leading and trailing whitespace is removed.
+     *
+     * @param message The message to display when prompting for input.
+     * @return The number entered by the user.
+     */
+    private static int promptUserForThreeDigitInt(String message)
+    {
+        Scanner scan           = new Scanner(System.in);
+        int     userInput      = 0;
+        boolean hasThreeDigits = false;
+
+        do {
+            queryUser(message);
+
+            while (!scan.hasNextInt()) {
+                queryUser(message);
+                scan.next();
+            }
+            userInput = scan.nextInt();
+
+            hasThreeDigits = String.valueOf(userInput).length() == 3;
+
+        } while (userInput <= 0 || !hasThreeDigits);
+
+        return userInput;
     }
 }
