70888a04bb0e747895c7ce87fd899e2be5a56732
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-07 13:37:05 +0100

    Add skeleton "L2 - 11. Växelpengar".

---
+++ src/js224eh_lab1/VaxelPengar.java
@@ -0,0 +1,43 @@
+package js224eh_lab1;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-07.
+ *
+ * Lektion 2 - (Input/Output, Operations on Primitive Types)
+ *
+ * 11. Växelpengar (VG-uppgift)
+ *     Då du handlar i en affär och betalar kontant får du kanske växel
+ *     tillbaka. I regel får affärsbiträdet hjälp av kassaapparaten med att
+ *     beräkna summan man ska få tillbaka. Vilka sedlar och mynt som ska lämnas
+ *     tillbaka får affärsbiträdet själv räkna ut i huvudet. Kan ett program
+ *     göra samma sak?
+ *
+ *     Skriv ett program VaxelPengar.java som beräknar den växel du får tillbaka
+ *     i samband med ett köp. Programmet ska, förutom att presentera beloppet
+ *     kunden får tillbaka avrundat till närmaste hela krona, även bestämma
+ *     vilka, och antalet, sedlar och mynt som kunden ska få tillbaka så att så
+ *     få sedlar och mynt används som möjligt. Programmet ska kunna ge växel
+ *     tillbaka med sedlar av valörerna 1000, 500, 100, 50 och 20 samt mynten
+ *     10, 5 och 1 krona. Du kan anta att det alltid finns tillräckligt antal av
+ *     de sedlar och mynt som krävs.
+ *
+ *         Ange kostnaden: 372,38
+ *         Ange erhållet belopp: 1000
+ *         Tillbaka: 628 kronor
+ *         1000-lappar: 0
+ *         500-lappar: 1
+ *         100-lappar: 1
+ *         50-lappar: 0
+ *         20-lappar: 1
+ *         10-kronor: 0
+ *         5-kronor: 1
+ *         1-kronor: 3
+ *
+ */
+public class VaxelPengar
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
