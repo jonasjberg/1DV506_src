313b00b64110cbc243f02a0ac965a4faae792adf
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-09 18:08:35 +0100

    Update ..

--- src/js224eh_lab1/Avstand.java
+++ src/js224eh_lab1/Avstand.java
@@ -20,42 +20,56 @@ public class Avstand
 {
     public static void main(String[] args)
     {
-        double coordX1 = 3.1415;
-        double coordY1 = 33;
+        double coordX1 = -3.1415;
+        double coordY1 = -33;
         double coordX2 = -10;
         double coordY2 = -1.3;
 
         SimpleVector coordFirst  = new SimpleVector(coordX1, coordY1);
         SimpleVector coordSecond = new SimpleVector(coordX2, coordY2);
 
-        double distance = getCoordinateDistance(coordFirst, coordSecond);
+        double distance = coordFirst.getDistance(coordSecond);
 
-        System.out.printf("Avstånd mellan koordinater %s och %s: %.2f",
+        System.out.printf("Avståndet mellan koordinaterna %s och %s är %.3f",
                           coordFirst.toString(), coordSecond.toString(),
                           distance);
-    }
-
-    private static double getCoordinateDistance(SimpleVector coordFirst,
-                                                SimpleVector coordSecond)
-    {
-        double x1 = coordFirst.x;
-        double y1 = coordFirst.y;
-        double x2 = coordSecond.x;
-        double y2 = coordSecond.y;
-        return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
     }
 }
 
 class SimpleVector
 {
+    // Getters and setters left out for brevity ..
     double x, y;
 
-    public SimpleVector(double x, double y)
+    /**
+     * A simple wrapper for two values representing a vector or 2D coordinate.
+     * @param x The X position of this vector/coordinate.
+     * @param y The Y position of this vector/coordinate.
+     */
+    SimpleVector(double x, double y)
     {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Calculates the distance between this vector (coordinate) and another
+     * vector (coordinate).
+     * Uses the formula:   distance = Sqrt( (x1-x2)^2 + (y1-y2)^2 )
+     *
+     * @param another Distance is calculated between this vector/coordinate
+     *                and the calling object.
+     * @return The distance between this object and the given SimpleVector.
+     */
+    double getDistance(SimpleVector another)
+    {
+        return Math.sqrt(Math.pow(this.x - another.x, 2) +
+                         Math.pow(this.y - another.y, 2));
+    }
+
+    /**
+     * @return A string representation of this object.
+     */
     @Override
     public String toString()
     {
