6a639183fa45e9c09a18009db9786d54ddbed106
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-10 13:49:45 +0100

    Fix 'Avstand.java' .. Seems to work OK.

--- src/js224eh_lab1/Avstand.java
+++ src/js224eh_lab1/Avstand.java
@@ -1,6 +1,6 @@
 package js224eh_lab1;
 
-import static js224eh_lab1.UserInputUtils.promptForTextLine;
+import java.util.Scanner;
 
 /*
  * Created by Jonas Sjöberg (js224eh) on 2016-11-09.
@@ -26,44 +26,111 @@ public class Avstand
 
     public static void main(String[] args)
     {
-        String input1 = promptForTextLine(MSG_QUERY_COORDINATE);
-        //String       input2      = promptForTextLine(MSG_QUERY_COORDINATE);
+        double[] coords1 = promptUserForCoordinates();
+        double[] coords2 = promptUserForCoordinates();
 
-        System.out.println("raw input: " + input1);
-        String cleanInput1 = cleanupString(input1);
-        System.out.println("cleaned input: " + cleanInput1);
+        SimpleVector firstCoordinate = createSimpleVector(coords1);
+        SimpleVector secondCoordinate = createSimpleVector(coords2);
 
-        SimpleVector firstCoordinate = createCoordinateFromString(cleanupString(input1));
-
-        double       coordX2          = 1;
-        double       coordY2          = 1;
-        SimpleVector secondCoordinate = new SimpleVector(coordX2, coordY2);
-
-        double distance = firstCoordinate.getDistance(secondCoordinate);
+        double distance = firstCoordinate.getDistanceTo(secondCoordinate);
 
         System.out.printf("Avståndet mellan koordinaterna %s och %s är %.3f",
                           firstCoordinate.toString(),
                           secondCoordinate.toString(), distance);
     }
 
+    /**
+     * Prompts the user for 'n' number of values, separated by commas.
+     *
+     * A message prompt is repeatedly displayed until the user input matches the
+     * conditions: User entered 'NUMBER_OF_VALUES' valid doubles, separated
+     * by commas. Everything but commas, periods, dashes and digits is removed
+     * prior to parsing the numbers.
+     *
+     * @return An array of integers with length 'NUMBER_OF_VALUES'.
+     */
+    private static double[] promptUserForCoordinates()
+    {
+        final int NUMBER_OF_VALUES = 2;
+        Scanner   scan             = new Scanner(System.in);
+        String[]  commaSepValues   = null;
+        String    userInput        = null;
+        boolean   inputValidated   = false;
+
+        do {
+            UserInputUtils.queryUser(MSG_QUERY_COORDINATE);
+            userInput = scan.nextLine();
+            userInput = cleanupString(userInput);
+            commaSepValues = userInput.split(",", NUMBER_OF_VALUES);
+
+            if (commaSepValues.length == NUMBER_OF_VALUES &&
+                stringArrayContainsOnlyValidDoubles(commaSepValues)) {
+                inputValidated = true;
+            }
+        } while (!inputValidated);
+
+        return convertStringArrayToDoubleArray(commaSepValues);
+    }
+
+    /**
+     * Converts an array of strings to an array of doubles.
+     *
+     * @param arrayToConvert The array of strings to convert.
+     * @return The resulting array of doubles.
+     */
+    private static double[] convertStringArrayToDoubleArray(String[] arrayToConvert)
+    {
+        double[] doubleArray = new double[arrayToConvert.length];
+
+        for (int i = 0; i < arrayToConvert.length; i++) {
+            try {
+                doubleArray[i] = Double.parseDouble(arrayToConvert[i]);
+            } catch (NumberFormatException e) {
+                /* TODO: Handle illegal array contents properly .. */
+            }
+        }
+
+        return doubleArray;
+    }
+
+    /**
+     * Tests that an array of strings contains only valid doubles. That is,
+     * strings that could be converted to doubles without throwing an exception.
+     *
+     * @param arrayToTest The array whose contents will be tested.
+     * @return True if the array contains only valid doubles, else False.
+     */
+    private static boolean stringArrayContainsOnlyValidDoubles(
+            String[] arrayToTest)
+    {
+        for (String arrayEntry : arrayToTest) {
+            try {
+                double number = Double.parseDouble(arrayEntry);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     private static String cleanupString(String string)
     {
-        // Remove everything except digits, commas and periods.
-        string = string.replaceAll("[^0-9,.]", "");
+        // Remove whitespace. Then remove everything that is not
+        // digits, dashes, commas or periods.
+        string = string.replaceAll("\\s+", "").replaceAll("[^0-9,.-]", "");
 
-        // Hacky workaround for locale-dependant decimal separator ..
-        string = string.replaceAll(",", ".");
+        // // Hacky workaround for locale-dependant decimal separator ..
+        // string = string.replaceAll(",", ".");
 
-        // Remove any leading and trailing periods.
-        string = string.replaceAll("^\\.+", "").replaceAll("\\.+$", "");
+        // // Remove any leading and trailing periods.
+        // string = string.replaceAll("^\\.+", "").replaceAll("\\.+$", "");
         return string;
     }
 
-    private static SimpleVector createCoordinateFromString(String string)
+    private static SimpleVector createSimpleVector(double[] values)
     {
-        double x = string.
-
-        return new SimpleVector(1, 2);
+        return new SimpleVector(values[0], values[1]);
     }
 
 }
@@ -95,7 +162,7 @@ class SimpleVector
      *                and the calling object.
      * @return The distance between this object and the given SimpleVector.
      */
-    double getDistance(SimpleVector another)
+    double getDistanceTo(SimpleVector another)
     {
         return Math.sqrt(Math.pow(this.x - another.x, 2) +
                          Math.pow(this.y - another.y, 2));
