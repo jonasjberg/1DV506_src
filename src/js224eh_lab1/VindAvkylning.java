43462debdc89d966e1e72b8ce82c2b65925f9158
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-10 15:32:36 +0100

    Add skeleton "L3 - 16. Vidavkylning" ..

---
+++ src/js224eh_lab1/VindAvkylning.java
@@ -0,0 +1,28 @@
+package js224eh_lab1;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-10.
+ *
+ * Lektion 3 - Using Library Classes
+ *
+ * 16. Vindavkylning, effektiv temperatur (VG-uppgift)
+ *     Du upplever säkert en lägre temperatur än vad termometern visar då det
+ *     blåser. Men vilken temperatur är det du upplever? Ett sätt att beräkna
+ *     den upplevda (eller effektiva) temperaturen är med hjälp av Siple's
+ *     formula.
+ *
+ *     Skriv ett program VindAvkylning.java där användaren genom att ange
+ *     temperaturen och vindhastigheten kan få reda på den effektiva
+ *     temperaturen, avrundad till närmsta heltal.
+ *
+ *         Ange temperatur: -7.8
+ *         Ange vindhastigheten: 8.4
+ *         Effektiv temperatur: -24 grader Celsius
+ */
+public class VindAvkylning
+{
+    public static void main(String[] args)
+    {
+        /* TODO: Implement .. */
+    }
+}
