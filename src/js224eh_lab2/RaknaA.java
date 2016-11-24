5427964d1138afef01d271e94388d4e318a7766f
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-24 14:50:05 +0100

    Start working on Lab #2.

---
+++ src/js224eh_lab2/RaknaA.java
@@ -0,0 +1,19 @@
+package js224eh_lab2;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-24.
+ *
+ * Lektion 4 - Styrande satser
+ *
+ * Uppgift 1
+ * Skriv ett program RaknaA.java som läser in en textrad från tangentbordet och
+ * sedan skriver hur ut många 'a' och 'A' det finns i raden.
+ * En körning kan se ut enligt följande:
+ *
+ *     Mata in en textrad: Alla barn fick högsta betyget A.
+ *     Antal a: 3
+ *     Antal A: 2
+ */
+public class RaknaA
+{
+}
