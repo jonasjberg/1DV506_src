72446a6fb3298962814b02f7e85f4df80938ad5d
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-26 00:41:47 +0100

    Start #5 ..

---
+++ src/js224eh_lab2/Triangle.java
@@ -0,0 +1,38 @@
+package js224eh_lab2;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-11-26.
+ *
+ * Lektion 4 - Styrande satser
+ * Uppgift 5
+ * Skriv ett program Triangle.java som för ett godtyckligt positivt udda heltal
+ * N (läses in från tangentbordet) skriver ut två olika trianglar.
+ * Först en rätvinklig sedan en likbent. Exempel på körning:
+ *
+ *     Mata in ett udda heltal: 7
+ *     Rätvinklig triangel:
+ *           *
+ *          **
+ *         ***
+ *        ****
+ *       *****
+ *      ******
+ *     *******
+ *
+ *     Likbent triangel:
+ *        *
+ *       ***
+ *      *****
+ *     *******
+ *
+ * Notera, heltalet N bestämmer hur lång basen på varje triangel är. Programmet
+ * skall avslutas med ett felmeddelande om det inmatade talet inte är ett
+ * positivt udda heltal.
+ */
+public class Triangle
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
