2873d9adb4f070f47b2690b2b20f47e7002731ed
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-26 00:38:31 +0100

    Various fixes and cleanup of 'HighLow.java'.

--- src/js224eh_lab2/HighLow.java
+++ src/js224eh_lab2/HighLow.java
@@ -2,9 +2,12 @@ package js224eh_lab2;
 
 import java.util.Random;
 
+import static js224eh_lab2.UserInputUtils.promptForPositiveWholeNumber;
+
 /*
  * Created by Jonas Sjöberg (js224eh) on 2016-11-24.
  *
+ * Lektion 4 - Styrande satser
  * Uppgift 4
  * Skriv ett program HighLow.java som implementerar gissningsleken High and
  * Low. Datorn väljer ett slumptal mellan 1 och 100 och låter sedan spelaren
@@ -20,27 +23,28 @@ import java.util.Random;
  *
  * Programmet avbryts efter 10 gissningar med en lämplig slutkommentar.
  */
-public class HighLow {
-    public static void main(String[] args) {
-        final int correctAnswer = getRandomNumber(1, 100);
-        int numberGuesses = 1;
-        int guessedNumber = UserInputUtils.promptForPositiveWholeNumber("Gissning " + numberGuesses + ": ");
+public class HighLow
+{
+    public static void main(String[] args)
+    {
+        final int correctAnswer   = getRandomNumber(1, 100);
+        int       guessCount = 1;
 
-        if (guessedNumber == correctAnswer) {
-            System.out.println("   Rätt svar efter bara " + numberGuesses + " gissningar - Strålande!");
+        int guess = promptForPositiveWholeNumber("Gissning " + guessCount + ": ");
+        if (guess == correctAnswer) {
+            System.out.printf("   Rätt svar efter bara %d gissningar - Strålande!", guessCount);
         }
 
-        while (guessedNumber != correctAnswer) {
-            numberGuesses++;
-            System.out.println("   Ledtråd: " + (guessedNumber > correctAnswer ? "lägre" : "högre"));
-            guessedNumber = UserInputUtils.promptForPositiveWholeNumber("Gissning " + numberGuesses + ": ");
+        while (guess != correctAnswer) {
+            guessCount++;
+            System.out.println("   Ledtråd: " + (guess > correctAnswer ? "lägre" : "högre"));
 
-            if (guessedNumber == correctAnswer) {
-                System.out.println("   Rätt svar efter bara " + numberGuesses + " gissningar - Strålande!");
+            guess = promptForPositiveWholeNumber("Gissning " + guessCount + ": ");
+            if (guess == correctAnswer) {
+                System.out.printf("   Rätt svar efter bara %d gissningar - Strålande!", guessCount);
                 break;
             }
-
-            if (numberGuesses == 10) {
+            if (guessCount == 10) {
                 System.out.println("   Max antal gissningar överskridet!");
                 break;
             }
@@ -56,10 +60,9 @@ public class HighLow {
      * @param max The maximum possible value of any single digit.
      * @return A random number within the range [min:max].
      */
-    private static int getRandomNumber(int min, int max) {
+    private static int getRandomNumber(int min, int max)
+    {
         Random random = new Random();
-        //return random.nextInt((max - min) + 1) + min)
-        int randomNumber = random.nextInt(((max - min) + 1) + min);
-        return randomNumber;
+        return random.nextInt(((max - min) + 1) + min);
     }
 }
