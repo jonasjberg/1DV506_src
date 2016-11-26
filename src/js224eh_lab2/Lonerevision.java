639dd4d010526479e3715f5acb6be702394da830
Jonas Sjöberg <jomeganas@gmail.com>
2016-11-26 23:34:30 +0100

    Work in progress #13 ..

--- src/js224eh_lab2/Lonerevision.java
+++ src/js224eh_lab2/Lonerevision.java
@@ -1,6 +1,7 @@
 package js224eh_lab2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /*
@@ -22,14 +23,12 @@ import java.util.Scanner;
  * medianlönen genom att medelvärdet av de två mittersta lönerna beräknas.
  * Två körningar kan se ut enligt följande:
  *
- *     Mata in löner (och avsluta med 'X'): 21700 28200 26300 25100 22600
- *     22800 19900 X
+ *     Mata in löner (och avsluta med 'X'): 21700 28200 26300 25100 22600 22800 19900 X
  *     Medianlön: 22800
  *     Medellön: 23800
  *     Lönespridning: 8300
  *
- *     Mata in löner (och avsluta med 'X'): 22100 29800 27300 25400 23100
- *     22300 X
+ *     Mata in löner (och avsluta med 'X'): 22100 29800 27300 25400 23100 22300 X
  *     Medianlön: 24250
  *     Medellön: 25000
  *     Lönespridning: 7700
@@ -42,7 +41,7 @@ public class Lonerevision
 
         /* Hämta text från användaren. */
         Scanner            scan                 = new Scanner(System.in);
-        ArrayList<Integer> salaries             = new ArrayList<Integer>();
+        ArrayList<Integer> salaries             = new ArrayList<>();
         boolean            shouldGetMoreNumbers = true;
 
         do {
@@ -60,6 +59,46 @@ public class Lonerevision
         } while (shouldGetMoreNumbers);
 
         scan.close();
+
+        /* Skriv ut resultatet. */
         System.out.println(salaries);
+        System.out.printf("Medianlön: %d%n", calculateMedian(salaries));
+        System.out.printf("Medellön: %d%n", calculateMean(salaries));
+        System.out.printf("Lönespridning: %d%n", calculateDistribution(salaries));
+        System.exit(0);
+    }
+
+    private static int calculateDistribution(ArrayList<Integer> salaries)
+    {
+        return 0;
+    }
+
+    private static int calculateMedian(ArrayList<Integer> numbers)
+    {
+        System.out.println("calculateMedian got: " + numbers);
+        Collections.sort(numbers);
+        System.out.println("sorted: " + numbers);
+
+        int midPos = numbers.size() / 2;
+
+        if (numbers.size() % 2 == 0) {
+            return (numbers.get(midPos) + numbers.get(midPos - 1)) / 2;
+        } else {
+            return numbers.get(midPos);
+        }
+    }
+
+    /**
+     * Calculates the mean of a ArrayList of integers.
+     * @param numbers The list of integers from which to calculate the mean.
+     * @return The mean of the numbers in the given list 'numbers'.
+     */
+    private static int calculateMean(ArrayList<Integer> numbers)
+    {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.size();
     }
 }
