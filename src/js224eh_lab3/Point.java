c009107d41756a233b5d1179c4717667da33420a
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-08 20:07:57 +0100

    Add Javadoc to 'Point.java'.

--- src/js224eh_lab3/Point.java
+++ src/js224eh_lab3/Point.java
@@ -9,38 +9,66 @@ public class Point
 {
     private int x, y;
 
+    /**
+     * Creates a new point at coordinates (0, 0).
+     */
     public Point()
     {
         this(0, 0);
     }
 
+    /**
+     * Creates a new point at coordinates (x, y).
+     * @param x The X-coordinate of the new point.
+     * @param y The Y-coordinate of the new point.
+     */
     public Point(int x, int y)
     {
-        this.x = x;
-        this.y = y;
+        moveToXY(x, y);
     }
 
+    /**
+     * @return The X-position of this point.
+     */
     public int getX()
     {
         return this.x;
     }
 
+    /**
+     * @return The Y-position of this point.
+     */
     public int getY()
     {
         return this.y;
     }
 
+    /**
+     * Tests if a point has the same coordinates as another point.
+     * @param another The second point to test against.
+     * @return True if the coordinates of both points are equal.
+     */
     public boolean isEqualTo(Point another)
     {
         return this.x == another.getX() && this.y == another.getY();
     }
 
+    /**
+     * Moves (translates) the point by the vector (x, y).
+     * @param x Size of horizontal movement (translation)
+     * @param y Size of vertical movement (translation)
+     */
     public void move(int x, int y)
     {
         this.x += x;
         this.y += y;
     }
 
+    /**
+     * Moves the point to coordinates (x, y).
+     * @param x The points new X-position.
+     * @param y The points new Y-position.
+     */
     public void moveToXY(int x, int y)
     {
         this.x = x;
