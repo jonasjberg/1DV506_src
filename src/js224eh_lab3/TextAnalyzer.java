27a5dfb39f940d9aeda41a7b06a93ba4e62b8c31
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-08 19:45:58 +0100

    Completed #5.

--- src/js224eh_lab3/TextAnalyzer.java
+++ src/js224eh_lab3/TextAnalyzer.java
@@ -7,4 +7,67 @@ package js224eh_lab3;
  */
 public class TextAnalyzer
 {
+    private String text;
+    private int charCount = 0;
+    private int upperCaseCount = 0;
+    private int whitespaceCount = 0;
+    private int digitCount = 0;
+
+    public TextAnalyzer(String text)
+    {
+        this.text = text;
+        runAnalysis();
+    }
+
+    /* Run the analysis just once at object instantiation. */
+    private void runAnalysis()
+    {
+        for (int i = 0; i < this.text.length(); i++) {
+            char c = this.text.charAt(i);
+
+            charCount++;
+            if (Character.isUpperCase(c)) {
+                upperCaseCount++;
+            }
+            else if (Character.isWhitespace(c)) {
+                whitespaceCount++;
+            }
+            else if (Character.isDigit(c)) {
+                digitCount++;
+            }
+        }
+    }
+
+    public int charCount()
+    {
+        return charCount;
+    }
+
+    public int upperCaseCount()
+    {
+        return upperCaseCount;
+    }
+
+    public int whitespaceCount()
+    {
+        return whitespaceCount;
+    }
+
+    public int digitCount()
+    {
+        return digitCount;
+    }
+
+    public boolean containsChar(char c)
+    {
+        /* ".. if no such character occurs in this string, then -1 is returned."
+         *  http://docs.oracle.com/javase/8/docs/api/java/lang/String.html#indexOf-int-
+         */
+        return text.indexOf(c) != -1;
+    }
+
+    public boolean containsString(String s)
+    {
+        return text.contains(s);
+    }
 }
--- src/js224eh_lab3/TextAnalyzerMain.java
+++ src/js224eh_lab3/TextAnalyzerMain.java
@@ -1,24 +1,43 @@
 package js224eh_lab3;
 
-/**
+/*
  * Created by Jonas Sjöberg (js224eh) on 2016-12-08.
+ *
+ * Laboration 3  --  Uppgift 5
  */
 public class TextAnalyzerMain
 {
     public static void main(String[] args)
     {
-        String text = "My name is Anakin Skywalker. My age is 42.";
-        TextAnalyzer ta = new TextAnalyzer(text);
+        String       text = "My name is Anakin Skywalker. My age is 42.";
+        TextAnalyzer ta   = new TextAnalyzer(text);
 
-        System.out.println("Char Count: "+ ta.charCount());
-        System.out.println("Upper Case Count: "+ ta.upperCaseCount());
-        System.out.println("Whitespace Count: "+ ta.whitespaceCount());
-        System.out.println("Digit Count "+ ta.digitCount());
+        System.out.println("Char Count: " + ta.charCount());
+        System.out.println("Upper Case Count: " + ta.upperCaseCount());
+        System.out.println("Whitespace Count: " + ta.whitespaceCount());
+        System.out.println("Digit Count " + ta.digitCount());
 
         if (ta.containsChar('x'))
             System.out.println("The text contains char \'x\'");
 
         if (ta.containsString("nakin"))
+            System.out.println("The text contains substring \"nakin\"");
+
+
+        System.out.println("-------------------------------------------------");
+
+        String       text2 = "";
+        TextAnalyzer ta2   = new TextAnalyzer(text2);
+
+        System.out.println("Char Count: " + ta2.charCount());
+        System.out.println("Upper Case Count: " + ta2.upperCaseCount());
+        System.out.println("Whitespace Count: " + ta2.whitespaceCount());
+        System.out.println("Digit Count " + ta2.digitCount());
+
+        if (ta2.containsChar('x'))
+            System.out.println("The text contains char \'x\'");
+
+        if (ta2.containsString("nakin"))
             System.out.println("The text contains substring \"nakin\"");
     }
 }
