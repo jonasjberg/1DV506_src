ee87d6b2bbbce9faacaaf0bd2c8d28108cf1efec
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-08 01:42:01 +0100

    Start work on 'Pnr.java'.

---
+++ src/js224eh_lab3/Pnr.java
@@ -0,0 +1,24 @@
+package js224eh_lab3;
+
+/*
+ * Created by Jonas Sjöberg (js224eh) on 2016-12-08.
+ *
+ * Laboration 3  --  Uppgift 2
+ */
+public class Pnr
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static String getFirstPart(String personal)
+    {
+        String firstPart = "";
+        if (personal.isEmpty()) {
+            return firstPart;
+        }
+
+        firstPart = personal.split()
+    }
+}
