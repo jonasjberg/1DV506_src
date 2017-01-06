701f012b3d1e479b3c1012ab0c27c12120312404
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-06 02:15:01 +0100

    Start work on Lab #4.

---
+++ src/js224eh_lab4/Histogram.java
@@ -0,0 +1,42 @@
+package js224eh_lab4;
+
+
+/**
+ * Created by Jonas Sjöberg (js224eh) on 2017-01-06.
+ *
+ * 1DV506 -- Problemlösning och Programmering, HT2016
+ * Laboration 4: Exceptions, IO och interface
+ *
+ * Uppgift 1
+ * Skriv ett program Histogram.java som läser ett godtyckligt antal heltal från
+ * en fil och sedan ritar upp ett histogram (stapeldiagram) för de av talen som
+ * är mellan 1 och 100. Notera: Alla talen i filen behöver ej vara inom
+ * intervallet [1,100]. En körning kan se ut enligt följande:
+ *
+ *     Läser heltal från filen: C:\Temp\heltal.dat
+ *     Antal i intervallet [1,100]: 46
+ *     Övriga: 16
+ *     Histogram
+ *     1  - 10  | ******
+ *     11 - 20  | ****
+ *     21 - 30  | **
+ *     31 - 40  | ***
+ *     41 - 50  | *******
+ *     51 - 60  | ****
+ *     61 - 70  | ***
+ *     71 - 80  | *********
+ *     81 - 90  | *****
+ *     91 - 100 | ***
+ *
+ * Notera 1: Du måste skapa din egen datafil. Vi förväntar oss en vanlig
+ *           textfil med ett heltal per rad.
+ * Notera 2: Datafilens sökväg (tex C:\Temp\heltal.dat) skall vara indata till
+ *           programmet (via String[] args).
+ */
+public class Histogram
+{
+    public static void main(String[] args)
+    {
+        // TODO: ..
+    }
+}
