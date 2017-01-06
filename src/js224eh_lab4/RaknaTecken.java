bb95985b7f990f591607bbe8ecd550dace832509
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-06 15:22:50 +0100

    Add skeleton for #2 ..

---
+++ src/js224eh_lab4/RaknaTecken.java
@@ -0,0 +1,40 @@
+package js224eh_lab4;
+
+/**
+ * Created by Jonas Sjöberg (js224eh) on 2017-01-06.
+ *
+ * 1DV506 -- Problemlösning och Programmering, HT2016
+ * Laboration 4: Exceptions, IO och interface
+ * ---------------------------------------------------------------------------
+ *
+ * Uppgift 2
+ * =========
+ * Skriv ett program RaknaTecken.java som räknar tecken av olika typer i en
+ * text som läses från en fil. Vi vill att ni skall ange hur många tecken som
+ * kan hittas i texten av följande kategorier.
+ *
+ *     - Stora bokstäver
+ *     - Små bokstäver
+ *     - "Whitespace" (Dvs, mellanslag, tab och return)
+ *     - Övriga tecken
+ *
+ * Sökvägen till den textfil som ni läser ifrån får vara hårdkodad i programmet.
+ * (Vi kommer att testa programmet på en egen textfil.) Ett exempel på en
+ * textfil är ChampagnenBlirDyrare. Det är en artikel om champagne hämtad från
+ * tidningen Dagens Nyheter. Vi rekommenderar dock att ni börjar med en lite
+ * mindre text.
+ *
+ * En körning med filen ChampagnenBlirDyrare som indata skall ge följande
+ * resultat:
+ *              Antal stora bokstäver: 52
+ *              Antal små bokstäver: 2063
+ *              Antal "whitespaces": 463
+ *              Antal övriga: 83
+ *
+ * Om ditt resultat inte överensstämmer exakt med ovanstående ska en skriftlig
+ * förklaring till skillnaderna inlämnas tillsammans med labben.
+ */
+
+public class RaknaTecken
+{
+}
