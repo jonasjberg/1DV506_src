df21f44b23f93110ba0d63ec07148fdc7cee4f6f
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-11 08:59:07 +0100

    First version of #1 done.

--- src/js224eh_tentamen/FileHandling.java
+++ src/js224eh_tentamen/FileHandling.java
@@ -8,23 +8,76 @@ import java.util.Scanner;
 
 /**
  * Created by Jonas Sjöberg (js224eh) on 2017-01-11.
+ *
+ * Uppgift 1
+ * Skriv ett program, FileHandling.java, som öppnar en textfil innehållande ord
+ * separerade med antingen mellanslag eller radslut. Skriv ut alla ord baklänges
+ * som uppfyller följande villkor:
+ * Kontrollsumman för det aktuella ordet skall vara större än 3000 och det
+ * föregående ordet får inte ha en kontrollsumma som överstiger 3000.
+ * där kontrollsumman för ett ord beräknas enligt
+ *
+ *     kontrollsumma =        SUM        (index + ) * ASCIIvärde
+ *                      för varje tecken
+ *
  */
 public class FileHandling
 {
-    static final String DEFAULT_PATH = "./FileHandling_test.txt";
+    static final String DEFAULT_PATH = "/home/jonas/LNU/1DV506_Problemlosning/src/1DV506/src/js224eh_tentamen/FileHandling_test.txt";
 
     public static void main(String[] args)
     {
-        String path;
+        ArrayList<String> words;
+
         if (args == null || args.length == 0) {
-            path = DEFAULT_PATH;
+            words = readWordsFromFile(DEFAULT_PATH);
         } else {
-            path = args[0];
+            words = readWordsFromFile(args[0]);
         }
 
-        ArrayList<String> words = readWordsFromFile(path);
+        words = filterWordsByChecksum(words);
+        reverseArrayList(words);
         System.out.println(words);
+    }
 
+    private static void reverseArrayList(ArrayList<String> words)
+    {
+        int firstPos = words.size() - 1;
+        for(int secondPos = 0; secondPos < firstPos; secondPos++) {
+            words.add(secondPos, words.remove(firstPos));
+        }
+    }
+
+    private static ArrayList<String> filterWordsByChecksum(ArrayList<String> words)
+    {
+        ArrayList<String> filteredWords = new ArrayList<>();
+
+        int previousChecksum = 0;
+        for (String word : words) {
+            int thisChecksum = calculateWordChecksum(word);
+
+            if (thisChecksum > 3000 && previousChecksum < 3000) {
+                filteredWords.add(word);
+            }
+
+            previousChecksum = thisChecksum;
+        }
+
+        return filteredWords;
+    }
+
+    /**
+     * Assumes ASCII encoded text which might not always be the case.
+     * Does conversion by casting characters to integers.
+     */
+    private static int calculateWordChecksum(String word)
+    {
+        int sum = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            sum += (i + 1) * (int) c;
+        }
+        return sum;
     }
 
     private static ArrayList<String> readWordsFromFile(String path)
