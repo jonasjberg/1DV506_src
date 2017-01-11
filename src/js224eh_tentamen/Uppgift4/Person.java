297791001b98c4b3aab570a57d9abbd423fc26df
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-11 12:03:29 +0100

    Solve #4 with way complicated solution.

--- src/js224eh_tentamen/Uppgift4/Person.java
+++ src/js224eh_tentamen/Uppgift4/Person.java
@@ -1,6 +1,5 @@
 package js224eh_tentamen.Uppgift4;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 
 
@@ -14,10 +13,17 @@ import java.util.Iterator;
 public class Person
 {
     final static int DEFAULT_AGE = -1;
+    final static int MAX_PERSONS = 100;
 
-    Person[] parents = new Person[2];
+    private static int[][] childLookup = new int[MAX_PERSONS][MAX_PERSONS];
+    private static int personCount = 0;
+    private static Person[] persons = new Person[MAX_PERSONS];
+
+    private Person[] parents = new Person[2];
     private int age;
     private String name;
+    public int id;
+    private int numberChildren = 0;
 
     public Person(String name)
     {
@@ -28,6 +34,8 @@ public class Person
     {
         this.age = age;
         this.name = name;
+        persons[personCount] = this;
+        this.id = personCount++;
     }
 
     /*
@@ -45,6 +53,7 @@ public class Person
         }
 
         parents[numberParents - 1] = p;
+        childLookup[p.id][numberChildren++] = this.id;
     }
 
     /*
@@ -52,33 +61,36 @@ public class Person
     * Notera att du måste skapa en separat iteratorklass som
     * vet hur man itererar över alla barnen!
     */
-    public Iterator<Person> getChildren()
+    Iterator<Person> getChildren()
     {
         class PersonIterator implements Iterator<Person>
         {
             private int nextChild;
-            private Person[] children;
+            private Person parent;
+            private int[][] childLookup;
 
-            public PersonIterator(Person[] allPersons)
+            public PersonIterator(Person parent, int[][] childLookup)
             {
-                this.allPersons = allPersons;
+                this.parent = parent;
+                this.childLookup = childLookup;
                 nextChild = 0;
             }
 
             @Override
             public boolean hasNext()
             {
-                return nextChild <= children.length;
+                return nextChild <= childLookup[parent.id].length;
             }
 
             @Override
             public Person next()
             {
-                return children.get(nextChild++);
+                int index = childLookup[parent.id][nextChild++];
+                return persons[index];
             }
         }
 
-        return new PersonIterator(allPersons);
+        return new PersonIterator(this, childLookup);
     }
 
 
